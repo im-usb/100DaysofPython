ef82d0231b9e9f8bf7fc5449d56373fe1ff058e3
uday singh baghel <58341470+im-usb@users.noreply.github.com>
2022-09-07 20:06:48 +0530

    Update

--- Day-4/Rock-Papper-Scissors/main.py
+++ Day-4/Rock-Papper-Scissors/main.py
@@ -32,40 +32,30 @@ usr_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 f
 comp_choice = random.randint(0,2)
 rps_list = [rock, paper, scissors]
 
-if(usr_choice == 0 and comp_choice == 1):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You lose")
-
-elif(usr_choice == 0 and comp_choice == 2):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You win")
-
-elif(usr_choice == 1 and comp_choice == 0):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You win")
-
-elif(usr_choice == 1 and comp_choice == 2):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You lose")
-
-elif(usr_choice == 2 and comp_choice == 0):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You lose")
-
-elif(usr_choice == 2 and comp_choice == 1):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("You win")
-
-elif(usr_choice == comp_choice ):
-    print(f"You chose \n {rps_list[usr_choice]}")
-    print(f"Computer chose \n {rps_list[comp_choice]}")
-    print("It's a tie")
-
-elif(usr_choice > 2 or usr_choice < 0):
+if(usr_choice > 2 or usr_choice < 0):
   print("Game Over! Please enter the right choice and try again.")
+
+else:
+    print(f"You chose \n {rps_list[usr_choice]}")
+    print(f"Computer chose \n {rps_list[comp_choice]}")
+    
+    if(usr_choice == 0 and comp_choice == 1):
+        print("You lose")
+
+    elif(usr_choice == 0 and comp_choice == 2): 
+        print("You win")
+
+    elif(usr_choice == 1 and comp_choice == 0):
+        print("You win")
+
+    elif(usr_choice == 1 and comp_choice == 2):
+        print("You lose")
+
+    elif(usr_choice == 2 and comp_choice == 0):
+        print("You lose")
+
+    elif(usr_choice == 2 and comp_choice == 1):
+        print("You win")
+    
+    elif(usr_choice == comp_choice ):
+        print("It's a tie")
