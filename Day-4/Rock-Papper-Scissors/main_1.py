19b2cb1b3d142aab7296478f8c567aa3bd9aef77
uday singh baghel <58341470+im-usb@users.noreply.github.com>
2022-09-07 20:13:15 +0530

    Initial Commit

---
+++ Day-4/Rock-Papper-Scissors/main_1.py
@@ -0,0 +1,55 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+usr_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors."))
+comp_choice = random.randint(0,2)
+rps_list = [rock, paper, scissors]
+
+if(usr_choice > 2 or usr_choice < 0):
+  print("Game Over! Please enter the right choice and try again.")
+
+else:
+    print(f"You chose \n {rps_list[usr_choice]}")
+    print(f"Computer chose \n {rps_list[comp_choice]}")
+    
+    if(usr_choice == 0 and comp_choice == 2): 
+        print("You win")
+
+    elif(usr_choice == 2 and comp_choice == 0):
+        print("You lose")
+
+    elif(usr_choice > comp_choice):
+        print("You win")
+    
+    elif(usr_choice == comp_choice ):
+        print("It's a tie")
+    
+    else:
+        print("You lose")
