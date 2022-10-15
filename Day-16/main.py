7c49a799ba1140640be75e2151d16dc325081e82
Uday <58341470+im-usb@users.noreply.github.com>
2022-10-15 23:06:51 +0530

    Initial Commit

---
+++ Day-16/main.py
@@ -0,0 +1,28 @@
+# oops basics
+# print("Hello World")
+from turtle import Turtle, Screen
+
+# Creating an object
+timmy = Turtle()
+
+# object attributes
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+
+# Creating an object
+my_screen = Screen()
+
+# object attributes
+print(my_screen.canvheight)
+
+# object methods
+my_screen.exitonclick()
+
+
+
+
+
+
+
+
